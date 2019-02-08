bfa76265b636a7f507fcfa9176f91c5893463e62
mirianfranca <mirian@bridge.ufsc.br>
2019-02-08 15:53:03 -0200

    Configuração do ambiente para executar o testes unitarios

---
+++ pastel/testes/testeunitario/exemplo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+// Teste exemplo para compreender a estrutura dos testes
+void main() {
+  test('my first unit test', () {
+    var answer = 42;
+    expect(answer, 42);
+  });
+
+}
+
+   
