ecf11333955648f3e49b71f31af4a3cbdde7b7a1
mirianfranca <mirian@bridge.ufsc.br>
2019-02-22 14:13:01 -0300

    test

---
+++ pastel/testes/testewidget/exemplo.dart
@@ -0,0 +1,11 @@
+//import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // Define a test. The TestWidgets function will also provide a WidgetTester
+  // for us to work with. The WidgetTester will allow us to build and interact
+  // with Widgets in the test environment.
+  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    // Test code will go here!
+  });
+}
